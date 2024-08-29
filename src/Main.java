d28df614da632df4b289880e2566509c66d92456
saez-tomas <saezjuantomas@outlook.com>
2024-08-29 16:59:43 -0300

    Menu switch case

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,95 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int opcion;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            System.out.println("Ingresa un número entre 1 y 23, o 25 para terminar:");
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+                case 11:
+
+                    break;
+                case 12:
+
+                    break;
+                case 13:
+
+                    break;
+                case 14:
+
+                    break;
+                case 15:
+
+                    break;
+                case 16:
+
+                    break;
+                case 17:
+
+                    break;
+                case 18:
+
+                    break;
+                case 19:
+
+                    break;
+                case 20:
+
+                    break;
+                case 21:
+
+                    break;
+                case 22:
+
+                    break;
+                case 23:
+
+                    break;
+
+                case 24:
+                    System.out.println("Programa terminado.");
+                    break;
+                    default:
+                    System.out.println("Opción inválida. Inténtalo de nuevo.");
+            }
+        } while (opcion != 24);
+
+        sc.close();
     }
 }
+
